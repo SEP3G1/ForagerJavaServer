21351d0d0f3d536a1590e9e6f71c5602773c1bba
detb <57094519+detb@users.noreply.github.com>
2020-10-28 13:15:31 +0100

    Commented

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,20 @@ public class Main {
     public static void main(String[] args) throws IOException
     {
         HttpClient client = new DefaultHttpClient();
+
+        // Creates the request for the HTTP server
         HttpGet request = new HttpGet("http://"+ Config.IP_T3 + ":" + Config.PORT_T3 + "/api/userlogin?userName=Oliver&pass=654321");
+
+        // Executes the HTTP server request
         HttpResponse response = client.execute(request);
+
+        // Get the value of the header in the HTTP request. Expected 200 OK
+        String status = String.valueOf(response.getStatusLine());
+        System.out.println(status);
+
+        // Reads the body of the HTTP response
         BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+
         String line = "";
         ObjectMapper mapper = new ObjectMapper();
         User user;
