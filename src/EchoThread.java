2dddeb3931444ccfbe555d372b12c20a5a15043d
detb <57094519+detb@users.noreply.github.com>
2020-11-04 16:00:32 +0100

    trying to submit listing

--- src/EchoThread.java
+++ src/EchoThread.java
@@ -9,11 +9,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.utils.HttpClientUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.nio.client.util.HttpAsyncClientUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
@@ -66,6 +72,7 @@ public class EchoThread extends Thread {
                 switch (r.get(0)){
                     case "search": toSend = search(r.get(1)); break;
                     case "login": toSend = login(r.get(1)); break;
+                    case "createlisting": toSend = createListing(r.get(1)); break;
                 }
 
                 // Sending
@@ -88,6 +95,38 @@ public class EchoThread extends Thread {
             }
         }
     }
+
+    private String createListing(String str) throws IOException
+    {
+        HttpClient client = new DefaultHttpClient();
+        System.out.println(str);
+        str = str.replaceAll("}","%7D");
+        str = str.replaceAll("\\{", "%7B");
+        str = str.replaceAll("\"", "");
+        String uri = ("http://"+ Config.IP_T3 + ":" + Config.PORT_T3 + "/api/listing?listingAsString=");
+        // Creates the request for the HTTP server
+        HttpPost request = new HttpPost(uri + str);
+
+        System.out.println(request.getMethod() + request.toString());
+        // Executes the HTTP server request
+        HttpResponse response = client.execute(request);
+
+        // Get the value of the header in the HTTP request. Expected 200 OK
+        String status = String.valueOf(response.getStatusLine());
+        System.out.println("Response: " + status);
+
+        // Reads the body of the HTTP response
+        BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+
+        String line = "";
+
+        while ((line = rd.readLine()) != null) {
+            System.out.println(line);
+            return line;
+        }
+        return "Not implemented";
+    }
+
     public String getJSONQuery(String str) throws JsonProcessingException
     {
         ArrayList<Listing> listings = new ArrayList<Listing>();
