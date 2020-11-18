140a008e04639e44cc2d9b76f6b3039c09fb3af4
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2020-11-18 13:28:09 +0100

    CompanyController klasse oprettet, men ikke implementeret.

--- src/Config/Config.java
+++ src/Config/Config.java
@@ -2,7 +2,7 @@ package Config;
 
 public class Config
 {
-  public static final String IP_T3 = "10.152.210.25";
+  public static final String IP_T3 = "10.152.222.56";
   public static final int PORT_T3 = 5001;
 
   public static final int PORT_T2 = 4343;
